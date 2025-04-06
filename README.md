52dab96a4bfa8e496a917ceaecbe8fd75c770407
Svante <svfa24@student.bth.se>
2025-04-06 08:20:59 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-<img src="/public/img/mvc.jpg" alt="MVC" width="200" />
+![MVC](/public/img/mvc_small.jpg)
+
+# MVC - Objektorienterade webbteknologier
+
+### Instruction on how to clone the repo and run the website
+
+- Install the lab environment
+    - [PHP](https://dbwebb.se/kurser/mvc-v2/labbmiljo/php)
+    - [Composer](https://dbwebb.se/kurser/mvc-v2/labbmiljo/php-composer)
+    - [Make] (https://dbwebb.se/kurser/mvc-v2/labbmiljo/make2)
+    - [Node.js](https://nodejs.org/)
+    - [Git](https://dbwebb.se/kurser/mvc-v2/labbmiljo/git)
+
+- Clone repo to local computer
+
+```bash
+git clone https://github.com/SvanteF/mvc-report
+
+cd mvc-report
+
+```
+- Run the application
+
+```bash
+symfony server:start
+
+```
+
+You can reach it here http://127.0.0.1:8000.
 
---
+++ src/Controller/ControllerJson.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ControllerJson
+{
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+        $number = random_int(1, 3);
+        $quote = "";
+        $date = date('l jS \of F Y h:i:s A');
+
+
+        switch($number) {
+            case 1:
+                $quote = "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.";
+                break;
+            case 2:
+                $quote = "Imagination is more important than knowledge.";
+                break;
+            case 3:
+                $quote = "I have no special talent. I am only passionately curious.";
+                break;
+        }
+
+        $data = [
+            'Quote of the day' => $quote,
+            'Date and timestamp' => $date,
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}

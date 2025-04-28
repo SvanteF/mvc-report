46d40e6363f132385b07c20ca8205255c84f83da
Svante <svfa24@student.bth.se>
2025-04-28 21:27:02 +0200

    Setup of phpunit plus test class CardTest added

---
+++ tests/Card/DeckOfCardsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test class for class DeckOfCards
+ */
+class DeckOfCardsTestTest extends TestCase 
+{
+
+    /**
+     * Construct object and verify that the object has the expected properties, including both arguments
+     */
+    public function testCreateObject()
+    {
+        
+    }
+}

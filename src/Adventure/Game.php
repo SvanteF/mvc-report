e6330e3e3088ae118a82bbb656a1227c8d6c824b
Svante <svfa24@student.bth.se>
2025-05-22 14:19:36 +0200

    Updates of physical objects in the game

--- src/Adventure/Game.php
+++ src/Adventure/Game.php
@@ -12,76 +12,84 @@ class Game
     private array $rooms = [];
     private Player $player;
 
+    /**
+     * Info about the rooms
+     */
+    private string $infoHallen = "Du befinner dig i hallen. Härifrån kan du nå alla sovrum. Finns det smutskläder här? Plocka upp dem i så fall.";
+    private string $infoViggosRum = "Här kommer text som handlar om Viggos rum";
+    private string $infoAmeliesRum = "Här kommer text som handlar om Amélies rum";
+    private string $infoFabiansRum = "Här kommer text som handlar om Fabians rum";
+    private string $infoGrovkok = "Här kommer text som handlar om grovköket";
+
+    /**
+     * Room images
+     */
+    private string $imageHallen = "img/hallen.png";
+    private string $imageViggosRum = "img/uml.png";
+    private string $imageAmeliesRum = "img/uml.png";
+    private string $imageFabiansRum = "img/uml.png";
+    private string $imageGrovkok = "img/uml.png";
+
     public function __construct(string $playerName)
     {
-
         /**
-         * Info about the rooms
-         */
-        $infoHallen = "Här kommer text som handlar om hallen";
-        $infoViggosRum = "Här kommer text som handlar om Viggos rum";
-        $infoAmeliesRum = "Här kommer text som handlar om Amélies rum";
-        $infoFabiansRum = "Här kommer text som handlar om Fabians rum";
-        $infoGrovkok = "Här kommer text som handlar om grovköket";
-
-        /**
-         * Room images
-         */
-        $imageHallen = "img/uml.png";
-        $imageViggosRum = "img/uml.png";
-        $imageAmeliesRum = "img/uml.png";
-        $imageFabiansRum = "img/uml.png";
-        $imageGrovkok = "img/uml.png";
-
-
-        /**
-         * Create 2 keys
+         * Create 5 keys
          */
         $keys = [];
-        for ($i = 0; $i < 2; $i++) {
+        for ($i = 0; $i < 5; $i++) {
             $keys[] = new Key();
         }
 
         /**
-         * Create 6 pieces of laundry
+         * Create 15 pieces of laundry
          */
         $laundries = [];
-        for ($i = 0; $i < 6; $i++) {
+        for ($i = 0; $i < 15; $i++) {
             $laundries[] = new Laundry();
         }
 
         /**
-         * Create 3 closets
+         * Create 5 closets
          */
         $closets = [];
         /*for ($i = 0; $i < 3; $i++) {
             $closets[] = new Closet($i);
         }*/
-        $closets[] = new Closet(1);
-        $closets[] = new Closet(2);
-        $closets[] = new Closet(3, 2);
+        $closets[] = new Closet(1, 1); // $closets[0]
+        $closets[] = new Closet(2, 2); // $closets[1]
+        $closets[] = new Closet(3, 3); // $closets[2]
+        $closets[] = new Closet(4, 4); // $closets[3]
+        $closets[] = new Closet(5, 5); // $closets[4]
 
 
         /**
          * Add laundry to closets
          */
         $closets[0]->addThing($laundries[0]);
-        $closets[2]->addThing($laundries[5]);
+        $closets[1]->addThing($laundries[1]);
+        $closets[1]->addThing($laundries[2]);
+        $closets[4]->addThing($laundries[3]);
+        $closets[4]->addThing($laundries[4]);
 
 
         /**
          * Add key to closet
          */
-        $closets[1]->addThing($keys[0]);
+        //
+        $closets[1]->addThing($keys[4]);
+        $closets[2]->addThing($keys[3]);
+        $closets[3]->addThing($keys[0]);
+        $closets[4]->addThing($keys[2]);
+
 
         /**
          * Create all 5 rooms
          */
-        $hallen = new Room('Hallen', [$laundries[1]], [], $infoHallen, $imageHallen);
-        $viggosRoom = new Room('Viggos rum', [$laundries[2], $keys[1]], [$closets[0]], $infoViggosRum, $imageViggosRum);
-        $ameliesRoom = new Room('Amélies rum', [$laundries[3]], [$closets[1]], $infoAmeliesRum, $imageAmeliesRum);
-        $fabiansRoom = new Room('Fabians rum', [$laundries[4]], [$closets[2]], $infoFabiansRum, $imageFabiansRum);
-        $grovkok = new Room('Grovkök', [], [], $infoGrovkok, $imageGrovkok);
+        $hallen = new Room('Hallen', [$laundries[5], $laundries[6], $laundries[7]], [], $this->infoHallen, $this->imageHallen);
+        $viggosRoom = new Room('Viggos rum', [$laundries[8]], [$closets[0], $closets[1]], $this->infoViggosRum, $this->imageViggosRum);
+        $ameliesRoom = new Room('Amélies rum', [$laundries[9], $laundries[10]], [$closets[2]], $this->infoAmeliesRum, $this->imageAmeliesRum);
+        $fabiansRoom = new Room('Fabians rum', [$laundries[11], $laundries[12], $laundries[13], $keys[1]], [$closets[3], $closets[4]], $this->infoFabiansRum, $this->imageFabiansRum);
+        $grovkok = new Room('Grovkök', [$laundries[14]], [], $this->infoGrovkok, $this->imageGrovkok);
 
         /**
          * Connect hallen to all other rooms
